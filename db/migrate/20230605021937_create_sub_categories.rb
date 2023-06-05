1b337ae833aaac5a391c154afbe637bf03708a58
Alejandro <martinezreyesalejandro.314@gmail.com>
2023-06-04 20:20:42 -0600

    Modelo SubCategoría Agregado

---
+++ db/migrate/20230605021937_create_sub_categories.rb
@@ -0,0 +1,11 @@
+class CreateSubCategories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :sub_categories do |t|
+      t.string :number
+      t.string :name
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
